21ddc75f92f572aa901f59aadfea748c02d980af
uimarshall <uimarshall@gmail.com>
2023-12-23 15:31:35 +0000

    Add basic syntax for Dart

---
+++ basic-dart-syntax/statements.dart
@@ -0,0 +1,9 @@
+// *****Statements*****
+// A statement is an instruction that declares a type or instructs the program to perform a task. A statement is always terminated by a semicolon (;). For example, the following program has 3 statements:
+
+void main() {
+  String message = 'Welcome to the world of Dart!'; // Statement 1
+
+  print(message); // Statement 2
+  print('Hello, World!'); // Statement 3
+}
---
+++ basic-dart-syntax/syntax.dart
@@ -0,0 +1,49 @@
+// *******WhiteSpaces*********
+
+// formatted - with whitespace
+void main() {
+  bool android = true;
+  if (android) {
+    print("Android");
+  }
+}
+
+
+// **********Blocks***********
+/* In Dart, a block is a sequence of zero or more statements. A block is surrounded by curly braces ({}). For example, you can group statements into a block as follows:
+
+{
+   String message = 'Welcome to Dart!';
+   print(message);
+}
+
+Unlike a statement, a block is not terminated by a semicolon (;)
+
+*/
+
+// ********Identifiers***********
+
+/*Identifiers are names that you assign to the variables, constants, functions, etc. In Dart, the names of identifiers follow these rules:
+
+The alphabetic ([a-z], [A-Z]) and underscore (_) characters can appear at any position.
+Digits (0-9) cannot be in the first position but everywhere else.*/
+
+// Identifiers are case-sensitive. For example, `message` and `Message` identifiers are different.
+
+// Keywords
+
+/*Keywords are names that have a special meaning to the Dart compiler. All keywords are reserved identifiers. Therefore, you cannot use them as the names of identifiers. For example, the following program has a compile-time error because `var` is a keyword:*/
+
+// var = 'Hello, World!'; // Error: Can't use 'var' here (reserved keyword).
+
+// Literals
+
+/* Literals are primitive values in the program. For example, an integer has the following literal:
+
+10
+Code language: Dart (dart)
+To form a literal string, you surround text with single quotes ('), double quotes ("), or triple quotes ("""). For example:
+
+'Welcome to Dart!'
+
+*/
